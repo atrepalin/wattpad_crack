a83e17ca96ee486e6d60968c3464baf7c0a97ac5
atrepalin <61793734+atrepalin@users.noreply.github.com>
2020-06-30 12:05:40 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ document.querySelectorAll('p')
     {
         if(e.parentElement.tagName == "PRE")
             text += e.textContent
-	});
+    });
 text = text.split("+").join("");
 var b = new Blob(["\ufeff", text], {encoding: "UTF-8", type: "text/plain;charset=UTF-8"});
 var url = URL.createObjectURL(b); 
