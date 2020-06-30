6bd4cd07b59966563a19f6f5e0ad2015110cadca
atrepalin <61793734+atrepalin@users.noreply.github.com>
2020-06-30 12:01:31 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
-var text="";
+var text = '';
 document.querySelectorAll('p')
-   .forEach(function(e){text+=e.textContent});
-text.replace("+","");
-var b = new Blob(["\ufeff", text],{encoding:"UTF-8",type:"text/plain;charset=UTF-8"});
+   .forEach(function(e)
+   {
+    if(e.parentElement.tagName == "PRE")
+        text += e.textContent
+	});
+text = text.split("+").join("");
+var b = new Blob(["\ufeff", text], {encoding: "UTF-8", type: "text/plain;charset=UTF-8"});
 var url = URL.createObjectURL(b); 
-window.open(url,"_blank","");
+window.open(url, "_blank", "");
